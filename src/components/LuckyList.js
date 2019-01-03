4b2af50a231512fd83bdd5890daade1e9f5a196c
Matt.Good <matt.good@planet.local>
2019-01-02 22:48:41 -0500

    refactored component from function to class refactored some of the functions to be functions of the class added some initial work for saving state on unmount and for reading state on mount

--- src/components/LuckyList.js
+++ src/components/LuckyList.js
@@ -4,48 +4,63 @@ import LuckySwitch from './LuckySwitch';
 import ReactVirtualizedTable from './VirtualizedReactTable';
 import monlist from '../data/pokemon.json';
 
+class LuckyList extends React.Component {
 
-function getMonRow(id) {
-  const name = monlist[id].name;
-  const dex  = monlist[id].dex;
-  const lucky = isLucky(name);
-  return { id, name, lucky, dex}
-}
+  state = {
 
-function isLucky(monName) {
-  return ( <LuckySwitch /> );
-}
+  };
 
-function LuckyList() {
-  return (
-    <Paper style={{ height: 550, width: '100%' }}>
-      <ReactVirtualizedTable
-        rowCount={monlist.length}
-        rowGetter={({ index }) => getMonRow(index)}
-        onRowClick={event => console.log(event)}
-        columns={[
-          {
-            width: 50,
-            flexGrow: 1.0,
-            label: 'Pokemon',
-            dataKey: 'name',
-          },
-          {
-            width: 50,
-            flexGrow: 1.0,
-            label: 'Lucky',
-            dataKey: 'lucky',
-          },
-          {
-            width: 120,
-            label: 'Dex ID',
-            dataKey: 'dex',
-            numeric: true,
-          }
-        ]}
-      />
-    </Paper>
-  );
+  componentDidMount = () => {
+    console.log('mount');
+  };
+
+  componentWillUnmount = () => {
+    console.log('unmount');
+  };
+
+  getMonRow = (id) => {
+    const name = monlist[id].name;
+    const dex  = monlist[id].dex;
+    const lucky = this.isLucky(name);
+    return { id, name, lucky, dex}
+  };
+
+
+  isLucky = (monName) => {
+    return ( <LuckySwitch /> );
+  };
+
+  render() {
+    return (
+      <Paper style={{ height: 550, width: '100%' }}>
+        <ReactVirtualizedTable
+          rowCount={monlist.length}
+          rowGetter={({ index }) => this.getMonRow(index)}
+          onRowClick={event => console.log(event)}
+          columns={[
+            {
+              width: 50,
+              flexGrow: 1.0,
+              label: 'Pokemon',
+              dataKey: 'name',
+            },
+            {
+              width: 50,
+              flexGrow: 1.0,
+              label: 'Lucky',
+              dataKey: 'lucky',
+            },
+            {
+              width: 120,
+              label: 'Dex ID',
+              dataKey: 'dex',
+              numeric: true,
+            }
+          ]}
+        />
+      </Paper>
+    );
+  };
 }
 
 export default LuckyList;
