2d84736d702b94884811156079dacf514b32c353
Matt.Good <matt.good@planet.local>
2019-01-10 23:25:37 -0500

    adding in filter functionality - can search for pokemon by name - can clear search

--- src/components/LuckyList.js
+++ src/components/LuckyList.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import FormControl from '@material-ui/core/FormControl';
 import LuckySwitch from './LuckySwitch';
 import ImageCard from './ImageCard';
 import ReactVirtualizedTable from './VirtualizedReactTable';
 import monlist from '../data/pokemon.json';
 
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+  textField: {
+    flexBasis: 200,
+  },
+});
+
 class LuckyList extends React.Component {
 
   state = {
-    lucky: []
+    filterQuery: '',
+    lucky: [],
+    masterMonList: {},
+    masterMonCount: 0
   };
+
+  /**
+   * Skip Stoarge list of state values that we don't want to add to storage
+   * @type {Array}
+   */
+  skipStorage = ["filterQuery", "masterMonList", "masterMonCount"]
 
   componentDidMount = () => {
     this.hydrateStateFromLocalStorage();
+    this.setState({ masterMonList: monlist });
+    this.setState({ masterMonCount: monlist.length });
     window.addEventListener(
       "beforeunload",
       this.saveStateToLocalStorage.bind(this)
@@ -39,6 +68,10 @@ class LuckyList extends React.Component {
       if (!localStorage.hasOwnProperty(stateKey)) {
         continue;
       }
+
+      if (this.skipStorage.includes(stateKey)) {
+        continue;
+      }
       // pull data from local storage.
       let storageValue = localStorage.getItem(stateKey);
 
@@ -61,12 +94,16 @@ class LuckyList extends React.Component {
     // TODO: revist - could be a lot of writes if this.state has many keys.
     // currently we are only using one key.
     for (let key in this.state) {
+      if (this.skipStorage.includes(key)) {
+        continue;
+      }
       localStorage.setItem(key, JSON.stringify(this.state[key]));
     }
   };
 
   getImageCard = (id, name) => {
     id = id.padStart(3,0);
+    // TODO: fix this this variant stuff
     let variant = '00';
     if (id === "327" || id === "386") {
       variant = '11';
@@ -94,8 +131,8 @@ class LuckyList extends React.Component {
    * @return {[type]}    [description]
    */
   getMonRow = (id) => {
-    const name = monlist[id].name;
-    const dex  = monlist[id].dex;
+    const name = this.state.masterMonList[id].name;
+    const dex  = this.state.masterMonList[id].dex;
     const lucky = this.isLucky(name);
     const image = this.getImageCard(dex.toString(), name);
     return { id, image, name, lucky, dex };
@@ -143,11 +180,60 @@ class LuckyList extends React.Component {
     );
   }
 
+  /**
+   * Filter Pokemon based off of input.
+   * @param  {string} searchString Search text
+   * @return {[type]}              [description]
+   */
+  handleFilterDataChange = searchString => {
+
+    const lowerCaseQuery = searchString.toLowerCase();
+    // console.log(searchString + " " + lowerCaseQuery);
+    let list = monlist.filter(
+      x => x['name'].toLowerCase().includes(lowerCaseQuery)
+    );
+    this.setState(
+      {
+        masterMonList: list,
+        masterMonCount: list.length,
+        filterQuery: searchString
+      }
+    );
+  }
+
   render() {
+    const { classes } = this.props;
     return (
+      <div>
+      <Paper style={{ height: 94, width: '100%'}}>
+        <FormControl className={classNames(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="filter-pokemon">FILTER</InputLabel>
+          <Input
+            id="filter-pokemon"
+            type="text"
+            value={this.state.filterQuery}
+            onChange={e => this.handleFilterDataChange(e.target.value)}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Clear Filter"
+                  onClick={e => {
+                      this.setState({filterQuery: ''});
+                      this.handleFilterDataChange("");
+                    }
+                  }
+                >
+                  <ClearIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </Paper>
+
       <Paper style={{ height: 560, width: '100%' }}>
         <ReactVirtualizedTable
-          rowCount={monlist.length}
+          rowCount={this.state.masterMonList.length}
           disableHeader={true}
           rowHeight={74}
           rowGetter={({ index }) => this.getMonRow(index)}
@@ -176,8 +262,13 @@ class LuckyList extends React.Component {
           ]}
         />
       </Paper>
+      </div>
     );
   };
 }
 
-export default LuckyList;
+LuckyList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LuckyList);
