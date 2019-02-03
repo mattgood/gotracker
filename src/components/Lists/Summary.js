3ef9cb6eff1ad31a754c3de2f44b8feceeda74f2
Matt.Good <matt.good@planet.local>
2019-02-03 18:13:11 -0500

    creating some inital code for the List Summary page

---
+++ src/components/Lists/Summary.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 1350,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`,
+  },
+});
+
+function generate(element) {
+  return [0, 1, 2].map(value =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
+
+class Summary extends React.Component {
+  state = {
+    dense: false,
+    secondary: false,
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { dense, secondary } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Paper style={{ height: '100%', width: '100%' }}>
+        <Grid container spacing={12}>
+      
+          <Grid item xs={12}>
+            <Typography variant="h6" className={classes.title}>
+              Avatar with text and icon
+            </Typography>
+            <div className={classes.demo}>
+              <List dense={dense}>
+                {generate(
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <FolderIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Single-line item"
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                    <ListItemSecondaryAction>
+                      <IconButton aria-label="Delete">
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>,
+                )}
+              </List>
+            </div>
+          </Grid>
+        </Grid>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+Summary.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Summary);
