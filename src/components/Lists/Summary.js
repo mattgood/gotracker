68287b30656520d8340b1870cd3e41d0793966f8
Matt.Good <matt.good@planet.local>
2019-06-16 08:27:11 -0400

    removing unused import

--- src/components/Lists/Summary.js
+++ src/components/Lists/Summary.js
@@ -6,7 +6,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import ListRow from '../Elements/ListRow';
-import { list } from 'postcss';
+
 
 const styles = theme => ({
   root: {
